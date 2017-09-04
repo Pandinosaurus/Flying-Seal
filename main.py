9d1d2fb68d917c74f26e1fa05fa659f574cb68ea
Rémi Ratajczak <Remi.Ratajczak@gmail.com>
2017-09-04 16:22:00 +0200

    Update main.py
    Added file info

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #File    : main.py
 #Author  : Remi Ratajczak
 #Contact : Remi.Ratajczak@gmail.com
+#
+#This is just an interface for the user
+#Go to game.py for the logic
 
 import os, sys, random
 from game import *      
